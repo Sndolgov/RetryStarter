6aeae1d6709732f149dd0a4c9fdf7d2dbe6f6186
sndolgov <sndolgov84@gmail.com>
2020-12-28 21:25:38 +0200

    added properties

--- src/main/java/com/starter/StarterConfiguration.java
+++ src/main/java/com/starter/StarterConfiguration.java
@@ -2,6 +2,7 @@ package com.starter;
 
 import com.starter.conditions.ConditionalOnProduction;
 import com.starter.properties.PropertiesExample;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +23,7 @@ public class StarterConfiguration {
 
     @Bean
     @ConditionalOnProduction
+    @ConditionalOnProperty("info.previous-versions")
     public ProductionNotifier getProductionNotifier(){
         return new ProductionNotifier();
     }
